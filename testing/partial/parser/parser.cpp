9ec282f545c068262b8287833b33a966dec687f2
GabrielM-512 <gabriel.maurer@gmx.de>
2026-09-07 22:06:53 +0200

    Add test for basic parser associativity

---
+++ testing/partial/parser/parser.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+
+#include "../../../src/AST/expr.h"
+#include "../../../src/util/fileIO.h"
+#include "../../../src/compiler/lexing.h"
+#include "../../../src/compiler/parsing/parsing.h"
+#include "../../../tool/prettyPrinter.h"
+
+
+static const char *filepath = "/home/gabriel/CLionProjects/grtlng-cpp/testing/partial/parser/test.gl";
+
+int main() {
+    std::string file = fileIO::readFile(filepath);
+    std::vector<Lexing::Tokens::Token> tokens = Lexing::scan(file);
+
+    Parsing::Parser parser(tokens);
+    Expr::Expr* expr = parser.parse();
+
+    std::string target = "( ( 16 + ( 5 * 3 ) ) - a )";
+    std::string got = Printer::print(expr);
+
+    if (target != got) {
+        std::cerr << "Parsing: Failed" << std::endl <<
+                     "    Expected: " << target << std::endl <<
+                     "    Got:      " << got << std::endl;
+        return 1;
+    }
+    std::cout << "Parsing: successful" << std::endl;
+    return 0;
+}
