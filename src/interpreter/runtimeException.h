ea8de1df1bcb7f9d656c3dfea1b0781ea7e7285e
GabrielM-512 <gabriel.maurer@gmx.de>
2026-09-14 11:19:49 +0200

    RuntimeException constructor without token

--- src/interpreter/runtimeException.h
+++ src/interpreter/runtimeException.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <stdexcept>
 
+#include "../compiler/lexing.h"
+
 namespace Interpreting {
     class RuntimeException : public std::runtime_error {
     public:
         std::string message;
         Lexing::Token token;
         RuntimeException(const std::string& message, Lexing::Token token) : std::runtime_error(message), message(message), token(token) {}
+        RuntimeException(const std::string& message) : std::runtime_error(message), message(message), token((Lexing::Token) {.type = Lexing::ERROR, .line = 0, .position = 0, .data = {nullptr}}) {}
     };
 }
