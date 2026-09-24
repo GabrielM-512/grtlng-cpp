6da702fa66e11cc5744caac2a50ebe62dcf3e011
GabrielM-512 <gabriel.maurer@gmx.de>
2026-09-24 22:33:16 +0200

    use parentheses for macro argument

--- src/value.h
+++ src/value.h
@@ -85,7 +85,7 @@ namespace Value {
     void defineNativesResolver(Resolving::Scope* scope);
 
 #define VALUE_NUM(number) ((Value::Value) {.type = Value::NUMBER, .as = {.num = (number)}})
-#define VALUE_CALLABLE(callable) ((Value::Value) {.type = Value::OBJECT, .as = {.object = callable}})
+#define VALUE_CALLABLE(callable) ((Value::Value) {.type = Value::OBJECT, .as = {.object = (callable)}})
 #define VALUE_TRUE (VALUE_NUM(1))
 #define VALUE_FALSE (VALUE_NUM(0))
 
